a951f27166ac1cf90b1d88bc9d98a871360fe7e3
Garion Opiola <garion-opiola@student.kirkwood.edu>
2023-02-21 18:57:11 -0600

    view team roles feature

---
+++ Extremely Casual Game Organizer/DataObjects/TeamRoles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataObjects
+{
+    public class TeamRoles
+    {
+        public int memberID { get; set; }
+        public int teamID { get; set; }
+        public string teamRoleTypeID { get; set; }
+    }
+}
